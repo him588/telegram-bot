57b37af5b11081e78277618f0ba51055122ee4d2
Himanshu <hk93931212@gmail.com>
2024-04-25 11:15:32 +0530

    deploy

---
+++ lib/bot.ts
@@ -0,0 +1,16 @@
+// const { Telegraf } = require("telegraf");
+import {Telegraf} from "telegraf"
+const TOKEN = process.env.local!;
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://celebrated-torte-184681.netlify.app/";
+
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+
+bot.launch();
